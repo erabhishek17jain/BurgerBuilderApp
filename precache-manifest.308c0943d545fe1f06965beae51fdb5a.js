f7d77191d994be6de84ebd131448f427e1dc777a
Abhishek <erabhishek17jain@gmail.com>
2020-10-09 06:12:52 +0530

    Updates

---
+++ precache-manifest.308c0943d545fe1f06965beae51fdb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f09455a59192adc56ca1181a90d498",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/index.html"
+  },
+  {
+    "revision": "8fc9743eba6dae4b3001",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/static/css/main.de0ebd47.chunk.css"
+  },
+  {
+    "revision": "0bb6e05c6dde1bb8451f",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/static/js/2.3a1e55a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/static/js/2.3a1e55a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc9743eba6dae4b3001",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/static/js/main.941f8521.chunk.js"
+  },
+  {
+    "revision": "e96796323a996630d114",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/static/js/runtime-main.fb46e8eb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/erabhishek17jain.github.io/BurgerBuilderApp/static/media/burger-logo.b8503d26.png"
+  }
+]);
